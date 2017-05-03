c4f5e469efb6c5de72d0aeccc5047a94b90d4de0
rmdm <rmdm@users.noreply.github.com>
2017-05-03 08:17:16 +0300

    simplify

--- src/index.js
+++ src/index.js
@@ -4,22 +4,9 @@ import assert from 'assert-match'
 import empower from 'empower'
 import formatter from 'power-assert-formatter'
 
-const empowerOptions = {
+const empoweredAssert = empower(assert, formatter(), {
     modifyMessageOnRethrow: true,
-    saveContextOnRethrow: true
-}
+    saveContextOnRethrow: true,
+})
 
-function customize (customOptions) {
-    const options = customOptions || {}
-    const empoweredAssert = empower(
-        assert,
-        formatter(options.output),
-        Object.assign({}, empowerOptions, options.assertion)
-    )
-    empoweredAssert.customize = customize
-    return empoweredAssert
-}
-
-const defaultAssert = customize()
-
-export default defaultAssert
+export default empoweredAssert
